eadbef7edb14d8a8d33e6f6822b54b3143b08347
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-08-17 00:02:35 -0300

    Dashboard structured and styled

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import { format, subDays, addDays } from "date-fns";
+import pt from "date-fns/locale/pt";
 
-import { Container } from "./styles";
+import api from "~/services/api";
+
+import { Container, Time } from "./styles";
 
 export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(
+    () => format(date, "d [de] MMMM", { locale: pt }),
+    [date]
+  );
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
+
   return (
     <Container>
-      <h1>DASHBOARD</h1>
+      <header>
+        <button type="button" onClick={handlePrevDay}>
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={handleNextDay}>
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Caio quirino</span>
+        </Time>
+        <Time>
+          <strong>09:00</strong>
+          <span>Caio quirino</span>
+        </Time>
+        <Time available>
+          <strong>10:00</strong>
+          <span>em aberto</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Caio quirino</span>
+        </Time>
+      </ul>
     </Container>
   );
 }
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -1,5 +1,47 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  height: 100%;
+  max-width: 600px;
+  margin: 50px auto;
+  display: flex;
+  flex-direction: column;
+
+  header {
+    display: flex;
+    align-self: center;
+    align-items: center;
+
+    strong {
+      color: #fff;
+      font-size: 24px;
+      margin: 0 15px;
+    }
+  }
+
+  ul {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 15px;
+    margin-top: 15px;
+  }
+`;
+
+export const Time = styled.li`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  border-radius: 4px;
+  background: #fff;
+  opacity: ${props => (props.past ? "0.6" : "1")};
+
+  strong {
+    color: ${props => (props.available ? "#999" : "#7159c1")};
+    font-size: 20px;
+    font-weight: normal;
+  }
+
+  span {
+    margin-top: 3px;
+    color: ${props => (props.available ? "#999" : "#666")};
+  }
 `;
